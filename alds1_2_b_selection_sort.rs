20d5ea24c52b58434417e9c7ce1361f544b7cbc3
Spxg <itsme@spxg.me>
2020-07-07 00:11:08 +0800

    selection sort

---
+++ alds1_2_b_selection_sort.rs
@@ -0,0 +1,19 @@
+fn selection_sort(vec: &mut Vec<i32>) {
+    for i in 0..(vec.len() - 1) {
+        let mut min_index = i;
+        for j in (i + 1)..vec.len() {
+            if vec[j] < vec[min_index] { min_index = j; }
+        }
+        vec.swap(i, min_index)
+    }
+}
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).expect("read error");
+    let mut numbers = buf.trim().split(" ").into_iter()
+                    .map(|num| num.parse::<i32>().expect("parse error"))
+                    .collect::<Vec<i32>>();
+    selection_sort(&mut numbers);
+    println!("{:?}", numbers);
+}
