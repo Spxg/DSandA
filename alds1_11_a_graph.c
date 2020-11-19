74ef741621b5b75994c6e13fba4ef999ee3aff63
Spxg <itsme@unsafe.me>
2020-11-19 17:01:37 +0800

    update

---
+++ alds1_11_a_graph.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MAX 10
+
+int main(int argc, char const *argv[])
+{
+    int martx[MAX][MAX] = { 0 };
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int node, degree;
+        scanf("%d %d", &node, &degree);
+        for (int i = 0; i < degree; i++) {
+            int v;
+            scanf("%d", &v);
+            martx[node - 1][v - 1] = 1; 
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d", martx[i][j]);
+            if (j != n - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
